b58e775660f8a830c28efd68fc01d13f7552be5d
anirbanghana <anirbanghana@gmail.com>
2023-11-27 19:48:34 +0530

    details to publish page-2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/11megablog/",
+  base:'/anirbanghana.github.io',
   plugins: [react()],
 })
